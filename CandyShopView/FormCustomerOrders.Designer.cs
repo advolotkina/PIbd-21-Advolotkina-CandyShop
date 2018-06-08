e735b8c5db00c2bcb84fa47d07d75b2fac872d4e
advolotkina <31734118+advolotkina@users.noreply.github.com>
2018-06-09 03:32:24 +0400

    Add files via upload

---
+++ CandyShopView/FormCustomerOrders.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CandyShopView
+{
+    partial class FormCustomerOrders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dateTimePickerFrom = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerTo = new System.Windows.Forms.DateTimePicker();
+            this.buttonMake = new System.Windows.Forms.Button();
+            this.buttonToPdf = new System.Windows.Forms.Button();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonToPdf);
+            this.panel1.Controls.Add(this.buttonMake);
+            this.panel1.Controls.Add(this.dateTimePickerTo);
+            this.panel1.Controls.Add(this.dateTimePickerFrom);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(571, 31);
+            this.panel1.TabIndex = 0;
+            // 
+            // dateTimePickerFrom
+            // 
+            this.dateTimePickerFrom.Location = new System.Drawing.Point(3, 4);
+            this.dateTimePickerFrom.Name = "dateTimePickerFrom";
+            this.dateTimePickerFrom.Size = new System.Drawing.Size(141, 20);
+            this.dateTimePickerFrom.TabIndex = 0;
+            // 
+            // dateTimePickerTo
+            // 
+            this.dateTimePickerTo.Location = new System.Drawing.Point(150, 4);
+            this.dateTimePickerTo.Name = "dateTimePickerTo";
+            this.dateTimePickerTo.Size = new System.Drawing.Size(151, 20);
+            this.dateTimePickerTo.TabIndex = 1;
+            // 
+            // buttonMake
+            // 
+            this.buttonMake.Location = new System.Drawing.Point(308, 3);
+            this.buttonMake.Name = "buttonMake";
+            this.buttonMake.Size = new System.Drawing.Size(96, 23);
+            this.buttonMake.TabIndex = 2;
+            this.buttonMake.Text = "Сформировать";
+            this.buttonMake.UseVisualStyleBackColor = true;
+            this.buttonMake.Click += new System.EventHandler(this.buttonMake_Click);
+            // 
+            // buttonToPdf
+            // 
+            this.buttonToPdf.Location = new System.Drawing.Point(448, 3);
+            this.buttonToPdf.Name = "buttonToPdf";
+            this.buttonToPdf.Size = new System.Drawing.Size(75, 23);
+            this.buttonToPdf.TabIndex = 3;
+            this.buttonToPdf.Text = "В PDF";
+            this.buttonToPdf.UseVisualStyleBackColor = true;
+            this.buttonToPdf.Click += new System.EventHandler(this.buttonToPdf_Click);
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "CandyShopView.ReportCustomerPurchaseOrders.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(16, 50);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.ServerReport.BearerToken = null;
+            this.reportViewer.Size = new System.Drawing.Size(568, 348);
+            this.reportViewer.TabIndex = 1;
+            // 
+            // FormCustomerOrders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 410);
+            this.Controls.Add(this.reportViewer);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormCustomerOrders";
+            this.Text = "FormCustomerOrders";
+            this.Load += new System.EventHandler(this.FormCustomerOrders_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonToPdf;
+        private System.Windows.Forms.Button buttonMake;
+        private System.Windows.Forms.DateTimePicker dateTimePickerTo;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFrom;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+    }
+}
--- CandyShopView/Program.cs
+++ CandyShopView/Program.cs
@@ -1,6 +1,9 @@
-﻿using CandyShopService.ImplementationsList;
-using CandyShopService.Interfaces;
+﻿using CandyShopService.Interfaces;
+using CandyShopService;
+using CandyShopService.ImplementationsBD;
+using CandyShopService.ImplementationsList;
 using System;
+using System.Data.Entity;
 using System.Windows.Forms;
 using Unity;
 using Unity.Lifetime;
@@ -25,13 +28,15 @@ namespace CandyShopView
         public static IUnityContainer BuildUnityContainer()
         {
             var currentContainer = new UnityContainer();
-            currentContainer.RegisterType<ICustomerService, CustomerServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IIngredientService, IngredientServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IConfectionerService, ConfectionerServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<ICandyService, CandyServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IWarehouseService, WarehouseServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IMainService, MainServiceList>(new HierarchicalLifetimeManager());
-            
+            currentContainer.RegisterType<DbContext, CandyShopDbContext>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<ICustomerService, CustomerServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IIngredientService, IngredientServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IConfectionerService, ConfectionerServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<ICandyService, CandyServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IWarehouseService, WarehouseServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IMainService, MainServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IReportService, ReportServiceBD>(new HierarchicalLifetimeManager());
+
             return currentContainer;
         }
     }
